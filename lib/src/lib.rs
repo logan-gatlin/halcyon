63d1da74e9e714c0e216245d390fa68fa1109985
Logan Gatlin <logan@gatlintc.com>
2025-09-05 10:57:29 -0500

    Fixed bug

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -81,7 +81,7 @@ pub fn compile(input: &str) -> std::result::Result<Vec<u8>, String> {
     //println!("FILE SIZE: {:.2} kb", (wasm.len() as f64) / 1024.0);
     let _wat = wasmprinter::print_bytes(&wasm).unwrap();
     //println!("{_wat}");
-    std::fs::write("./demo.wat", &_wat).unwrap();
+    //std::fs::write("./demo.wat", &_wat).unwrap();
     wasmparser::validate(&wasm)
         .map_err(|e| format!("COMPILER BUG: WASM failed validation with error:\n{e}"))?;
     Ok(wasm)
